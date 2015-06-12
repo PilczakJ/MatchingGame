6c878c3b94b7b489f1d32da5460918e55f1dab5d
John Pilczak <johnnypilz@yahoo.com>
2015-06-12 14:00:06 -0400

    Initial Commit
    A scene with a object that appears and disappears every two seconds
    along with UI

---
+++ Assets/ScoreKeeping.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreKeeping : MonoBehaviour {
+
+	private Text txt;
+	private int score = 0;
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text = "Score: " + score;
+
+	}
+
+	public void SetScore(int val) {
+		score = val;
+	}
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	public ScoreKeeping scoreKeeper;
+	private float time;
+	private bool shown;
+	// Use this for initialization
+	void Start () {
+		shown = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if(!shown && (int)time == 2)
+			ShowPossibleTarget ();
+		if ((int)time == 2 && shown)
+			RemovePossibleTarget ();
+		scoreKeeper.SetScore ((int)time);
+	}
+
+	void ShowPossibleTarget()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+		shown = true;
+		time = 0;
+	}
+
+	void RemovePossibleTarget()
+	{
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		time = 0;
+		shown = false;
+	}
+}
