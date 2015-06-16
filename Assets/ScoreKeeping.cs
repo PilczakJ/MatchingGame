1c7cb7fb8d72fb5a258730bc961a6f2d6ebeb761
John Pilczak <johnnypilz@yahoo.com>
2015-06-15 20:56:27 -0400

    Commenting
    Fully commented the scripts

--- Assets/ScoreKeeping.cs
+++ Assets/ScoreKeeping.cs
@@ -1,29 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
+// Basically the scorekeeper is the whole of the UI and scorekeeping
 public class ScoreKeeping : MonoBehaviour {
 
+	// A reference to the target
 	public Target t;
+
+	// Text to show the best reaction time for the current trial
 	private Text reactionText;
+
+	// Text to show whether it is currently showing the target
 	private Text targetText;
+
+	// Text to show how many trials you have selected
 	private Text trialsText;
+
+	// Text to show what trial it is currently
 	private Text currentTrialText;
+
+	// Text to show the number missed
 	private Text missedText;
+
+	// Title text
 	private Text title;
+
+	// Buttons to adjust the number of trials selected
 	private Button increaseTrials;
 	private Button decreaseTrials;
+
+	// Button to start the game
 	private Button startButton;
-	public float[] score;
-	public float[] missed;
+
+	// Reaction time and misses per trial
+	private float[] reaction;
+	private float[] missed;
+
+	// Total trials and current trial
 	private int trials = 1;
 	private int currentTrial = 1;
+
+	// Whether we are at the score screen currently
 	bool atEndScreen = false;
+
+	// A reference to the score screen's Canvas
 	private Canvas endScreen;
 
 	// Use this for initialization
 	void Start () {
+		// Instantiate missed/reaction to avoid null reference exeptions in update
 		missed = new float[trials];
-		score = new float[trials];
+		reaction = new float[trials];
+
+		// Set each UI element variable
 		endScreen = transform.FindChild ("End Score").GetComponent<Canvas> ();
 		reactionText = transform.FindChild("Score").GetComponent<Text> ();
 		targetText = transform.FindChild("Target Indicator").GetComponent<Text> ();
@@ -34,11 +64,14 @@ public class ScoreKeeping : MonoBehaviour {
 		currentTrialText = transform.FindChild ("Current Trial").GetComponent<Text> ();
 		missedText = transform.FindChild ("Missed 1").GetComponent<Text> ();
 		title = transform.FindChild ("Title").GetComponent<Text> ();
+
+		// Disable the UI not used on the title screen
 		reactionText.enabled = false;
 		targetText.enabled = false;
 		currentTrialText.enabled = false;
 		missedText.enabled = false;
 
+		// Set the buttons' onClick
 		increaseTrials.onClick.AddListener(() => {
 			trials++;
 		});
@@ -53,11 +86,14 @@ public class ScoreKeeping : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		reactionText.text = "Best Reaction Time: " + score[currentTrial-1];
+		// Keep the text updated
+		reactionText.text = "Best Reaction Time: " + reaction[currentTrial-1];
 		missedText.text = "Number Missed " + missed [currentTrial-1];
 		targetText.text = "Target";
 		trialsText.text = "Trials: " + trials;
 		currentTrialText.text = "Trial #" + currentTrial;
+
+		// Check for space on the score screen to return to the title screen
 		if (atEndScreen) {
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				endScreen.enabled = false;
@@ -69,13 +105,15 @@ public class ScoreKeeping : MonoBehaviour {
 
 	}
 
-	public void AddScore(float val) {
-		score[currentTrial-1] += val;
-	}
-	
+	// Getters/Setters for the current trial's misses and reactions
+	public float Missed{get{return missed[currentTrial-1];} set{missed[currentTrial-1] = value;}}
+	public float Reaction{get{return reaction[currentTrial-1];} set{reaction[currentTrial-1] = value;}}
+
+	// Other getters/setters
 	public int CurrentTrial{ get{return currentTrial;}set{currentTrial = value;}}
 	public Canvas EndScreen{ get { return endScreen; } }
 
+	// Toggle the target text
 	public void ToggleTargetText()
 	{
 		if (targetText.enabled)
@@ -84,10 +122,14 @@ public class ScoreKeeping : MonoBehaviour {
 			targetText.enabled = true;
 	}
 
+	// Start the game
 	void Begin()
 	{
+		// Set the missed/reaction arrays to the correct capacity
 		missed = new float[trials];
-		score = new float[trials];
+		reaction = new float[trials];
+
+		// Remove the UI from the main menu
 		increaseTrials.enabled = false;
 		increaseTrials.GetComponent<Image> ().enabled = false;
 		increaseTrials.transform.GetChild (0).GetComponent<Text> ().enabled = false;
@@ -103,11 +145,14 @@ public class ScoreKeeping : MonoBehaviour {
 		title.enabled = false;
 		currentTrialText.enabled = true;
 		missedText.enabled = true;
+
+		// Start the game
 		t.Begin (trials);
 	}
 
 	public void EndTrials()
 	{
+		// Replace the main menu UI
 		increaseTrials.enabled = true;
 		increaseTrials.GetComponent<Image> ().enabled = true;
 		increaseTrials.transform.GetChild (0).GetComponent<Text> ().enabled = true;
@@ -121,29 +166,35 @@ public class ScoreKeeping : MonoBehaviour {
 		reactionText.enabled = true;
 		currentTrialText.enabled = true;
 		title.enabled = true;
-		//targetText.enabled = true;
+		currentTrialText.enabled = false;
 	}
 
 	public void SwapToEndUI()
 	{
+		// Score screen UI
 		atEndScreen = true;
 		GetComponent<Canvas> ().enabled = false;
 		endScreen.enabled = true;
 		Text accuracy = endScreen.transform.FindChild ("Missed").GetComponent<Text> ();
-		Text reaction = endScreen.transform.FindChild ("Reaction").GetComponent<Text> ();
+		Text reactionScore = endScreen.transform.FindChild ("Reaction").GetComponent<Text> ();
 		Text finalScore = endScreen.transform.FindChild ("Final Score").GetComponent<Text> ();
 		accuracy.text = "Accuracy: \n";
-		reaction.text = "Best Reaction: \n";
+		reactionScore.text = "Best Reaction: \n";
 		finalScore.text = "Final Score: \n";
+
+		// Calculate accuracy
 		for (int i = 0; i<missed.Length; i++)
 			accuracy.text += "Trial " + (i + 1) + ": " + (1-missed [i])*100 + "%   ";
-		for (int j = 0; j<score.Length; j++) {
-			reaction.text += "Trial " + (j + 1) + ": " + score [j] + "  ";
-			if(score[j] == 0)
-				score[j] = 1;
+
+		// Get best reaction times
+		for (int j = 0; j<reaction.Length; j++) {
+			reactionScore.text += "Trial " + (j + 1) + ": " + reaction [j] + "  ";
+			if(reaction[j] == 0)
+				reaction[j] = 1;
 		}
-		for(int k = 0; k<score.Length; k++)
-			finalScore.text += "Trial " + (k + 1) + ": " + (int)(1/score[k] * (1-missed [k])*100) + "  ";
+		// The final score is calculated by multiplying 1/ReactionTime by the accuracy percentage (100% = 100)
+		for(int k = 0; k<reaction.Length; k++)
+			finalScore.text += "Trial " + (k + 1) + ": " + (int)(1/reaction[k] * (1-missed [k])*100) + "  ";
 		
 	}
 }
--- Assets/Target.cs
+++ Assets/Target.cs
@@ -3,33 +3,58 @@ using System.Collections;
 
 public class Target : MonoBehaviour {
 
+	// The scorekeeper to report score changes to
 	public ScoreKeeping scoreKeeper;
+
+	// The total number of trials
 	public int trials;
+
+	// The current trial
 	private int currentTrial = 1;
+
+	// The amount of time that has passed since the last action (show/remove image)
 	private float time;
+
+	// If an image is currently being shown
 	private bool imageShown;
+
+	// Whether the game has started
 	private bool started;
+
+	// Whether the initial target has been shown this trial
 	private bool targetShown;
+
+	// The number in the image series that it is on
 	private int imageSeriesNum = 0;
+
+	// Which image is the target
 	private int targetIndex;
+
+	// The array of indexes which tell which image to show in the series
 	private int[] trialImages;
+
+	// How many times space has been hit this trial (used for accuracy)
 	private int spaceCount = 0;
+
 	// Use this for initialization
 	void Start () {
+		// Instantiate the trialImage array and make all the images disabled to start
 		trialImages = new int[6];
 		for(int i = 0;i<transform.childCount;i++)
 			transform.GetChild(i).gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+
+		// The game hasn't started yet
 		started = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Keep track of the time since the last action and the current trial always
 		if (started) {
 			time += Time.deltaTime;
 			scoreKeeper.CurrentTrial = currentTrial;
 		}
-		//If the last trial is done, end the game
-
 
 		// If the trial has started and the initial target hasn't been shown, show the target for two seconds
 		if (started && currentTrial<=trials && !targetShown) {
@@ -47,101 +72,149 @@ public class Target : MonoBehaviour {
 				ShowPossibleTarget ();
 			if ((int)time == 2 && imageShown && imageSeriesNum < 6)
 				RemovePossibleTarget ();
+			//If the last image in the series has been shown, move on to the next trial
 			else if(imageSeriesNum == 6 && (int)time == 2)
 				NewTrial();
 
+			// If an image is shown...
 			if(imageShown)
 			{
+				// If space is detected...
 				if(Input.GetKeyDown(KeyCode.Space))
 				{
+					// increment the space count
 					spaceCount++;
+
+					// If it is the target, get the reaction time and move on to the next image
 					if(trialImages[imageSeriesNum] == targetIndex)
 					{
-						if(scoreKeeper.score[currentTrial-1] > (time) || scoreKeeper.score[currentTrial-1] == 0)
-							scoreKeeper.score[currentTrial-1] = (time);
+						if(scoreKeeper.Reaction > (time) || scoreKeeper.Reaction == 0)
+							scoreKeeper.Reaction = (time);
 						RemovePossibleTarget();
 					}
+
+					// If not, it is a miss
 					else
-						scoreKeeper.missed[currentTrial-1]++;
+						scoreKeeper.Missed++;
 				}
 			}
 		}
-
-
 	}
 
+	// Show an image
 	void ShowPossibleTarget()
 	{
 		transform.GetChild(trialImages[imageSeriesNum]).gameObject.GetComponent<SpriteRenderer> ().enabled = true;
 		imageShown = true;
+
+		// Reset the time since the last action
 		time = 0;
 	}
 
+	// Remove an image
 	void RemovePossibleTarget()
 	{
-		transform.GetChild(trialImages[imageSeriesNum]).gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		// If the image was the target and space wasn't hit, it is a miss
 		if (trialImages [imageSeriesNum] == targetIndex && targetShown) {
 			if(time > 1.1)
-				scoreKeeper.missed[currentTrial-1]++;
+				scoreKeeper.Missed++;
 		}
+
+		// Reset the time since the last action
 		time = 0;
 		imageShown = false;
+
+		// If it is the target, just remove it, if not, remove and move on to the next image
 		if (targetShown) {
+			transform.GetChild(trialImages[imageSeriesNum]).gameObject.GetComponent<SpriteRenderer> ().enabled = false;
 			imageSeriesNum++;
 		} else
 			transform.GetChild(targetIndex).gameObject.GetComponent<SpriteRenderer> ().enabled = false;
 
 	}
 
+	// Start a new trial
 	void NewTrial()
 	{
+		// The new trial's target has not been shown
 		targetShown = false;
+
+		// Reset the images
 		imageSeriesNum = 0;
-		if (spaceCount == 0 && scoreKeeper.missed [currentTrial - 1] == 0)
-			scoreKeeper.missed [currentTrial - 1] = 0;
-		else if (spaceCount == 0 && scoreKeeper.missed [currentTrial - 1] > 0)
-			scoreKeeper.missed [currentTrial - 1] = 1;
+
+		// Calculate the percent missed
+		if (spaceCount == 0 && scoreKeeper.Missed == 0)
+			scoreKeeper.Missed = 0;
+		else if (spaceCount == 0 && scoreKeeper.Missed > 0)
+			scoreKeeper.Missed = 1;
 		else
-			scoreKeeper.missed[currentTrial-1] = (scoreKeeper.missed[currentTrial-1]/spaceCount);
+			scoreKeeper.Missed = (scoreKeeper.Missed/spaceCount);
 		spaceCount = 0;
+
+		// If it is the last trial, end the game. If not, move on to the next trial
 		if (currentTrial == trials && started) {
 			EndTrials ();
 		} else {
+			// Make a new target and image series
 			NewTarget();
+
+			// Show the target text and the target
 			scoreKeeper.ToggleTargetText ();
 			time = 0;
 			transform.GetChild(targetIndex).gameObject.GetComponent<SpriteRenderer> ().enabled = true;
 			imageShown = true;
+
+			// Increment the trial
 			currentTrial++;
 		}
 	}
 
+	// End the game and move to the score screen
 	void EndTrials()
 	{
 		started = false;
-		//scoreKeeper.EndTrials();
 		scoreKeeper.SwapToEndUI ();
 	}
 
+	// Start the game
 	public void Begin(int trials)
 	{
 		started = true;
+
+		// Make a random target and image series
 		NewTarget ();
+
+		// It is the first trial
 		currentTrial = 1;
-		scoreKeeper.score [currentTrial - 1] = 0;
-		scoreKeeper.missed [currentTrial - 1] = 0;
+
+		// Set the scores to 0
+		scoreKeeper.Reaction = 0;
+		scoreKeeper.Missed = 0;
+
+		// The target has not been shown
 		targetShown = false;
+
+		// Show the target text
 		scoreKeeper.ToggleTargetText ();
+
+		// Show the target image
 		transform.GetChild(targetIndex).gameObject.GetComponent<SpriteRenderer> ().enabled = true;
 		imageShown = true;
 		time = 0;
+
+		// Set the total number of trials
 		this.trials = trials;
 
 	}
 
+	// Create a new randomized target and array of images
 	void NewTarget()
 	{
+		// The target is one of the six possible images
 		targetIndex = Random.Range (1, 6);
+
+		// Set a random image for each image in the series
+		// This means there can be one, multiple, or no targets in a trial
 		for(int i = 0;i<6;i++)
 		{
 			trialImages[i] = Random.Range(1,6);
